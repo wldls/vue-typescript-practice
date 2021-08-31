46e9168fb0d9065b9af777e996d9ec82795982f3
jiin <dslet2343@gmail.com>
2021-09-01 00:14:32 +0900

    할 일 관리 앱 - 추가 기능 구현

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <label for="todo-input">오늘 할 일 : </label>
+    <input type="text" id="todo-input" :value="item" @input="handleInput" />
+    <button @click="addTodo" type="button">추가</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    item: { type: String, required: true },
+  },
+  methods: {
+    handleInput(event: InputEvent) {
+      // 타입 단언을 사용
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit("input", eventTarget.value);
+    },
+    addTodo() {
+      this.$emit("add");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
