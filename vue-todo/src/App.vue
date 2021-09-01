a0431db2aff2ee2985823f6037aa9487007bbb56
jiin <dslet2343@gmail.com>
2021-09-01 17:35:49 +0900

    할 일 관리 앱 - 조회, 삭제 기능 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,23 +1,51 @@
 <template>
   <div>
-    <h1>Vue Todo with Typescript</h1>
-    <TodoInput
-      :item="todoText"
-      @add="addTodoItem"
-      @input="updateTodoText"
-    ></TodoInput>
+    <header><h1>Vue Todo with Typescript</h1></header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @add="addTodoItem"
+        @input="updateTodoText"
+      ></TodoInput>
+    </main>
+    <div>
+      <ul>
+        <TodoListItem
+          v-for="(todoItem, index) in todoItmes"
+          :key="index"
+          :todoItem="todoItem"
+          :index="index"
+          @remove="removeTodoItem"
+        ></TodoListItem>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItmes: any[]) {
+    const parsed = JSON.stringify(todoItmes);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
 
 export default Vue.extend({
-  components: { TodoInput },
+  components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
+      todoItmes: [] as any[],
     };
   },
   methods: {
@@ -26,13 +54,25 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-      localStorage.setItem(value, value);
+      this.todoItmes.push(value);
+      storage.save(this.todoItmes);
+      // localStorage.setItem(STORAGE_KEY, value);
 
       this.initTodoText();
     },
     initTodoText() {
       this.todoText = "";
     },
+    fetchTodoItems() {
+      this.todoItmes = storage.fetch();
+    },
+    removeTodoItem(index: number) {
+      this.todoItmes.splice(index, 1);
+      storage.save(this.todoItmes);
+    },
+  },
+  created() {
+    this.fetchTodoItems();
   },
 });
 </script>
